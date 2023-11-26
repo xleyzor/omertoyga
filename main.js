77266b7dae3c1fbb9ee612d3fbcfd3cc98d222cc
Ömer Toyga <122406455+xleyzor@users.noreply.github.com>
2023-11-26 23:07:48 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const circle = document.querySelector(".circle");
-const github = document.getElementById("github");
-const instagram = document.getElementById("instagram");
-const gmail = document.getElementById("gmail");
-circle.addEventListener("click", () => {
-  github.classList.toggle("show1");
-  gmail.classList.toggle("show2");
-  instagram.classList.toggle("show3");
-});
